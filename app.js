743420e07f55d1f0488cc6d049c66f33dcaba175
Hyacinthax <jc.dumogho@gmail.com>
2021-05-22 09:30:47 +1200

    changed port to PORT

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', (req,res,next) => {
     res.send('Welcome to the homepage');
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log("Fuck AWS");
 });
